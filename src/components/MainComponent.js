f2119a5e5e603124397fa2385fd4df6bff4b9393
Jahedul Anowar <1530887@iub.edu.bd>
2021-06-20 00:09:44 +0900

    SPA Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,10 +13,8 @@ import { PROMOTIONS } from '../shared/promotions';
 import { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-
 class Main extends Component {
-    constructor(props) 
-    {
+    constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
@@ -26,30 +24,50 @@ class Main extends Component {
         };
     }
 
-    
-    render() 
-    {
+    render() {
         const HomePage = () => {
-            return(
-                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
+            return (
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
                     promo={this.state.promotions.filter((promo) => promo.featured)[0]}
                     leader={this.state.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
-        }
+        };
+
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail
+                    dish={
+                        this.state.dishes.filter(
+                            (dish) => dish.id === parseInt(match.params.dishId, 10)
+                        )[0]
+                    }
+                    comments={this.state.comments.filter(
+                        (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+                    )}
+                />
+            );
+        };
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
-                    <Route exact path="/contactus" component={Contact}/>
+                    <Route
+                        exact
+                        path="/menu"
+                        component={() => <Menu dishes={this.state.dishes} />}
+                    />
+                    <Route exact path="/menu/:dishId" component={DishWithId} />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
-                </Switch>     
+                </Switch>
                 <Footer />
             </div>
         );
-    }  
+    }
 }
 
 export default Main;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,38 +1,48 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-
-
-    function RenderMenuItem({ dish, onClick })
-    {
-        return(
-            <Card onClick={() => onClick(dish.id)}> 
-                <CardImg width="100%"  src={dish.image} alt={dish.name} />
-                
-                <CardImgOverlay body className="ml-5">
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card>
+            <Link to={`/menu/${dish.id}`}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
-            </Card>
-        );
-    }
-    const Menu = (props) => {
-        // render a list of dishes
-        const menu = props.dishes.map((dish) => {
-            return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    {/* functional component */}
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />                          
-                </div>
-            );
-        });
+            </Link>
+        </Card>
+    );
+}
 
+const Menu = (props) => {
+    // render a list of dishes
+    const menu = props.dishes.map((dish) => {
         return (
-            <div class="container">
-                <div className="row">
-                    {menu}
-                </div>
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                {/* functional component */}
+                <RenderMenuItem dish={dish} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div class="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to="/home">Home</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">{menu}</div>
+        </div>
+    );
+};
 
 export default Menu;
